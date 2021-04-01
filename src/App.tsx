aeb8381df63bf0838280074dd0d54e56eb81d64a
Lucas Lobo <lucas5798fell@gmail.com>
2021-04-01 13:28:59 +0100

    Add folder structure and react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link 
+} from 'react-router-dom';
+
+import Example from './pages/example/Example'
+import Landing from './pages/landing/Landing'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/example">Example</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/example">
+            <Example />
+          </Route>
+          <Route path="/">
+            <Landing />
+          </Route>
+        </Switch>
+      </div>
+
+    </Router>
+  )
 }
 
 export default App;
--- src/pages/example/Example.test.tsx
+++ src/pages/example/Example.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Example from './Example';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Example />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
